942e0fd9f8db79ba3c237ccc3fa7ba4cb45facc1
Samuel Owusu <kwadwoamo10@gmail.com>
2019-09-23 23:43:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 
  Whatever you say to grandma, she should respond with
 if  grandma == HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+ puts unless you shout it (type in all capitals).
 
- If you shout, she can hear you (or at least she thinks so) 
- and yells back
+ If grandma == "you shout, she can hear you (or at least she thinks so) 
+ and yells back"
 elsif
  NO, NOT SINCE 1938!
 
