17e0e125c5b327fee4d6373795e593cb6c2233be
Samuel Owusu <kwadwoamo10@gmail.com>
2019-09-23 23:51:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
  grandma = "speak_to_grandma" 
 
- Whatever you say to grandma, she should respond with
-if  grandma == HUH?! SPEAK UP, SONNY!
- puts "HUH?! SPEAK UP, SONNY!" unless 
+ # Whatever you say to grandma, she should respond with
+if  grandma == "I'm going out"
+ puts "HUH?! SPEAK UP, SONNY!"  
 
  If grandma == "you shout, she can hear you (or at least she thinks so) 
  and yells back"
