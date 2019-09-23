dfa069573520e5b364ce08a9cbc2b18603fec534
Samuel Owusu <kwadwoamo10@gmail.com>
2019-09-23 23:47:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ if  grandma == HUH?! SPEAK UP, SONNY!
  If grandma == "you shout, she can hear you (or at least she thinks so) 
  and yells back"
 elsif
- NO, NOT SINCE 1938!
+ puts NO, NOT SINCE 1938!
 
 else However if you say 'I LOVE YOU GRANDMA!', she should respond with
 puts 'I LOVE YOU TOO PUMPKIN!'
